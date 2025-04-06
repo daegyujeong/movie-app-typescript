75e2df05f1ecfb825c74e4807b251364e3cf85b0
DaegyuJeong <k5946k@gmail.com>
2025-04-07 01:09:24 +0900

    Add global styles

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,50 @@
+import { createGlobalStyle } from "styled-components";
+import { Theme } from "./theme";
+
+interface GlobalStyleProps {
+  theme: Theme;
+}
+
+const GlobalStyle = createGlobalStyle<GlobalStyleProps>`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Roboto', sans-serif;
+    background-color: ${({ theme }) => theme.colors.background};
+    color: ${({ theme }) => theme.colors.text};
+    overflow-x: hidden;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  button {
+    cursor: pointer;
+    border: none;
+    background: none;
+    font-family: inherit;
+  }
+
+  ul, ol {
+    list-style: none;
+  }
+
+  img {
+    max-width: 100%;
+    display: block;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-weight: 700;
+  }
+
+  /* Add any other global styles here */
+`;
+
+export default GlobalStyle;
